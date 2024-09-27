e4e5a2fe13eef672eba232477510fde0818079ee
khushalGrover <khushal15grover@gmail.com>
2024-09-27 14:42:48 +0530

    config file changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'R3F-Easyjob',
+  base: 'R3F-Easyjob2',
   plugins: [react()],
   server: {
     host: true
