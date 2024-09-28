b353230bb69565265609e5eac33d9effdc51a3b6
khushalGrover <khushal15grover@gmail.com>
2024-09-28 11:40:47 +0530

    roll back

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Experience } from "./components/Experience";
 function App() {
 	return (
 		<>
-			hello start
+			hello easyjob2
 			<Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
 				<color attach="background" args={["#505000"]} />
 				<Experience />
