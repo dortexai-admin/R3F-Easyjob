b4b311b910e7cad2ec6b30029a13c95a35a4b9ce
khushalGrover <khushal15grover@gmail.com>
2024-10-15 00:57:51 +0530

    background animation added

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -81,14 +81,17 @@ export const UI = () => {
         </div>
       </main>
 
-      {/* <div className="fixed inset-0 flex items-center -rotate-2 select-none">
+       <div className="fixed inset-0 flex items-center -rotate-2 select-none">
         <div className="relative font-rubic">
           <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Khushal Grover
+              DortexAI
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
               Ui/Ux Designer
+            </h2>
+            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+              Digital Marketer
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
               Web Designer
@@ -103,10 +106,13 @@ export const UI = () => {
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Khushal Grover
+              DortexAI
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
               Ui/Ux Designer
+            </h2>
+            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
+              Digital Marketer
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
               Web Designer
@@ -123,7 +129,7 @@ export const UI = () => {
             
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 };
